15a35e49d44d694105e87e0aaca6778f10285764
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-07 17:26:21 +0200

    task1 done

--- js/task1.js
+++ js/task1.js
@@ -2,9 +2,9 @@
 
 const name = '«Генератор защитного поля»'; // Write code on this line
 let price = 1000; // Write code on this line
-const firstMessage = ``; // Write code on this line
+const firstMessage = `Выбран` + ` ` + name + `, цена за штуку` + ` ` + price + ` ` + `кредитов`; // Write code on this line
 price = 2000;
-const secondMessage = ``; // Write code on this line
+const secondMessage = `Выбран` + ` ` + name + `, цена за штуку` + ` ` + price + ` ` + `кредитов`; // Write code on this line
 
 console.log(firstMessage);
 // 'Выбран «Генератор защитного поля», цена за штуку 1000 кредитов'
